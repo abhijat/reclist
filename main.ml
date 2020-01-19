e72c3f82e5ca47b7671182357d773aed07b73fef
Abhijat Malviya <malviya.abhijat@gmail.com>
2020-01-19 20:49:02 +0530

    Removes useless code

--- main.ml
+++ main.ml
@@ -1,21 +1,5 @@
 open Core_kernel
 
-let make_csv words =
-  let rec inner words =
-    match words with
-    | [] -> ""
-    | [elem] -> elem
-    | first :: rest -> first ^ "," ^ inner rest
-  in
-  "'" ^ (inner words) ^ "'"
-
-let map_stuff () =
-  let data = Map.of_alist_exn (module String)
-               ["foo", 1; "bar", 2; "baz", 3] in
-  let data = Map.add_exn data ~key:"abhijat" ~data:37 in
-  print_endline (string_of_int (Map.find_exn data "abhijat"));
-  print_endline (string_of_int (Map.length data))
-
 let rec analyze_path path =
   let entries = Sys.readdir path in
   Array.iter ~f:(fun entry -> analyze_entry path entry) entries
